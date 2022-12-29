30d20b593ca209c88ff52f3cd9d8b310018acb23
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-28 21:11:14 -0600

    Committing Something

--- script (copy).js
+++ script (copy).js
@@ -1,33 +1,36 @@
 fetch("https://handlers.education.launchcode.org/static/astronauts.json")
 .then(function(response) {
   response.json().then(function(json) {
+
     let container = document.getElementById('container');
     container.innerHTML = `<p>Astronaut Count: ${json.length}</p>`
 
-    function sortHours(astronauts) {
+    function sortHours (astronauts) {
       let hoursArray = [];
-      for (let i in astronauts)
+      for (let i in astronauts) {
         hoursArray.push(json[i].hoursInSpace);
+      }
       return hoursArray.sort((a, b) => b - a);
     }
 
     let sortedArray = sortHours(json);
-
+      
     while (sortedArray.length > 0) {
       for (let i in json) {
         if (json[i].hoursInSpace === sortedArray[0]) {
-        container.innerHTML += `
+        container.innerHTML += ` 
         <div class="astronaut">
-          <div class="bio">
-            <h3>${json[i].firstName} ${json[i].lastName}</h3>
-            <ul>
-              <li>Hours in space: ${json[i].hoursInSpace}</li>
-              <li style="color:green;">Active: ${json[i].active}</li>
-              <li>Skills: ${json[i].skills}</li>
-            </ul>
+            <div class="bio">
+              <h3>${json[i].firstName} ${json[i].lastName}</h3>
+              <ul>
+                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                <li>Active: ${json[i].active}</li>
+                <li>Skills: ${json[i].skills}</li>
+              </ul>
+            </div>
+            <img class="avatar" src=${json[i].picture}>
           </div>
-          <img class="avatar" src=${json[i].picture}>
-        </div>`
+        `
         sortedArray.shift()
         };
       };
